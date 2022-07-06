c1a8208f7d2ad26c9475c5156376da632edd1e9c
Imarushenko <igormarushenko@gmail.com>
2022-07-06 15:42:57 +0300

    edit

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import ShowAnimals from "./ShowAnimals";
 
 export default function Animals() {
+
+  const [animals, setAnimals] = useState([]);
+
+  // read external data
+  useEffect(() => {
+    const getAnimales = async (db) => {
+      // use the collection in the firestore
+      const animalsCollection = collection(db, "Animals");
+      // gets all the docs of the collection
+      const animalsSnapshot = await getDocs(animalsCollection);
+      const animalsList = await animalsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      }));
+      setAnimals(animalsList);
+    };
+    getAnimales(db);
+  }, [animals]);
+
+
+  // function getUserChoise() {
+  //   let animalTypeArray = document.getElementsByName('animalType')
+  //   for (const animalType of animalTypeArray) {
+  //     setAnimals({ animalType: animalType.value })
+    
+  //   }
+  // }
+
+
   return (
     <>
       <div>
@@ -12,10 +44,10 @@ export default function Animals() {
           <h3>Here you can choose which animals you desire to see between the following categories from our zoo</h3>
           <hr />
           <Link to='/landAnimals'>
-            <button className="btn btn-warning" style={{ marginRight: "2em", width: 200, height: 50, border: "solid black 2px" }} value="landAnimals">For Land Animals</button>
+            <button className="btn btn-warning" style={{ marginRight: "2em", width: 200, height: 50, border: "solid black 2px" }} value="landAnimals" name="animalType">For Land Animals</button>
           </Link>
-          <button className="btn btn-info" style={{ marginRight: "2em", width: 200, height: 50, border: "solid black 2px" }} value="landAnimals">For Sea Animals</button>
-          <button className="btn btn-success" style={{ marginRight: "2em", width: 200, height: 50, border: "solid black 2px" }} value="landAnimals">For Air Animals</button>
+          <button className="btn btn-info" style={{ marginRight: "2em", width: 200, height: 50, border: "solid black 2px" }} value="seaAnimals" name="animalType">For Sea Animals</button>
+          <button className="btn btn-success" style={{ marginRight: "2em", width: 200, height: 50, border: "solid black 2px" }} value="airAnimals" name="animalType">For Air Animals</button>
         </div>
       </div>
       <Footer />
